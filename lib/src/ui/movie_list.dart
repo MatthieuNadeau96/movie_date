f0586887c58972be85ad11f8d173e5696065f6aa
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-28 16:51:15 -0400

    added icon buttons

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -27,31 +27,83 @@ class MovieList extends StatelessWidget {
     CardController controller;
     var results = snapshot.data.results;
     var deviceSize = MediaQuery.of(context).size;
-    return Container(
-      height: deviceSize.height * 0.75,
-      width: deviceSize.width,
-      child: TinderSwapCard(
-        orientation: AmassOrientation.BOTTOM,
-        totalNum: snapshot.data.results.length,
-        stackNum: 2,
-        swipeEdge: 6.0,
-        maxHeight: deviceSize.height * 0.6,
-        maxWidth: deviceSize.width * 0.75,
-        minHeight: deviceSize.height * 0.58,
-        minWidth: deviceSize.width * 0.7,
-        cardBuilder: (context, index) => MovieCard(index, results),
-        cardController: controller = CardController(),
-        swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
-          /// Get swiping card's alignment
-          if (align.x < 0) {
-            //Card is LEFT swiping
-          } else if (align.x > 0) {
-            //Card is RIGHT swiping
-          }
-        },
-        swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
-          /// Get orientation & index of swiped card!
-        },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              Text(
+                'Movie Date.',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.autorenew),
+                onPressed: () {},
+              )
+            ],
+          ),
+          Container(
+            height: deviceSize.height * 0.65,
+            width: deviceSize.width,
+            child: TinderSwapCard(
+              orientation: AmassOrientation.BOTTOM,
+              totalNum: snapshot.data.results.length,
+              stackNum: 3,
+              swipeEdge: 6.0,
+              maxHeight: deviceSize.height * 0.6,
+              maxWidth: deviceSize.width * 0.75,
+              minHeight: deviceSize.height * 0.58,
+              minWidth: deviceSize.width * 0.7,
+              cardBuilder: (context, index) => MovieCard(index, results),
+              cardController: controller = CardController(),
+              swipeUpdateCallback:
+                  (DragUpdateDetails details, Alignment align) {
+                /// Get swiping card's alignment
+                if (align.x < 0) {
+                  //Card is LEFT swiping
+                } else if (align.x > 0) {
+                  //Card is RIGHT swiping
+                }
+              },
+              swipeCompleteCallback:
+                  (CardSwipeOrientation orientation, int index) {
+                /// Get orientation & index of swiped card!
+              },
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(Icons.thumb_down),
+                onPressed: () {},
+                iconSize: 45,
+              ),
+              IconButton(
+                icon: Icon(Icons.bookmark),
+                onPressed: () {},
+                iconSize: 35,
+              ),
+              IconButton(
+                icon: Icon(Icons.thumb_up),
+                onPressed: () {},
+                iconSize: 45,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
